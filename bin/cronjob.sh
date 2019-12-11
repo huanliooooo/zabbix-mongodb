c2f7d6d87562fb0546fb8bf68cbb2933d475efe7
huanlio <danhhuan93@gmail.com>
2019-12-11 10:54:04 +0700

    remove

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -9,8 +9,8 @@ source $FILE_CONF
 
 echo `date` $SERVER $HOST_NAME
 chmod +x $FILE_EXECUTE
-croncmd1="$HOME_PATH/$FILE_EXECUTE $SERVER $HOST_NAME $HOME_PATH 1"
-croncmd2="$HOME_PATH/$FILE_EXECUTE $SERVER $HOST_NAME $HOME_PATH 2"
+croncmd1="$HOME_PATH/$FILE_EXECUTE $SERVER $HOST_NAME 1"
+croncmd2="$HOME_PATH/$FILE_EXECUTE $SERVER $HOST_NAME 2"
 cronjobFirst="* * * * * $croncmd1"
 cronjobSecond="* * * * * sleep 30; $croncmd2"
 ( crontab -l | grep -v -F "$croncmd1" ; echo "$cronjobFirst" ) | crontab -
